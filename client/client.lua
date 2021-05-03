e8958d3c6cb366a0889712dfdc6014703b6a4635
FARID <64978877+MrJiiz@users.noreply.github.com>
2021-05-04 02:31:45 +0430

    Add files via upload

---
+++ client/client.lua
@@ -0,0 +1,22 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+RegisterNetEvent("fz:delallveh")
+AddEventHandler("fz:delallveh", function ()
+    for vehicle in EnumerateVehicles() do
+        if (not IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1))) then 
+            SetVehicleHasBeenOwnedByPlayer(vehicle, false) 
+            SetEntityAsMissionEntity(vehicle, false, false) 
+            DeleteVehicle(vehicle)
+            if (DoesEntityExist(vehicle)) then 
+                DeleteVehicle(vehicle) 
+            end
+        end
+    end
+end)
